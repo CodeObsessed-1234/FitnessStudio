2a189acb8753c353b2e4235c0445ed72e228b5dc
USER <devanshsaxena12b1@gmail.com>
2024-01-26 16:12:27 +0530

    I have changed some code.

--- app/src/main/java/com/example/fitnessstudio/MainActivity.java
+++ app/src/main/java/com/example/fitnessstudio/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.fitnessstudio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Button button=this.findViewById(R.id.button);
+		button.setOnClickListener(event-> Toast.makeText(this,"Hello from me",Toast.LENGTH_SHORT).show());
 	}
 }
