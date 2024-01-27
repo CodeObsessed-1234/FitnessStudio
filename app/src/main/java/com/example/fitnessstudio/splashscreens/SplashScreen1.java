6a3a6e45cf67923f61bcbd043df2b81d95ac04b6
USER <devanshsaxena12b1@gmail.com>
2024-01-27 23:27:18 +0530

    Created package splashscreen and added SplashScreen1.java with exact fonts ( downloaded and some changes in xml.)

---
+++ app/src/main/java/com/example/fitnessstudio/splashscreens/SplashScreen1.java
@@ -0,0 +1,24 @@
+package com.example.fitnessstudio.splashscreens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.fitnessstudio.R;
+
+public class SplashScreen1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen1);
+        TextView textView=this.findViewById(R.id.skip_text_view_splash_screen1);
+        textView.setOnClickListener(event->skipButtonClicked(this.findViewById(R.id.skip_text_view_splash_screen1)));
+    }
+    public void skipButtonClicked(View view){
+        Toast.makeText(this,"Skip button pressed",Toast.LENGTH_SHORT).show();
+    }
+}
