424d8b9e6e6c3ab06dacb07fa347b15691373c79
USER <devanshsaxena12b1@gmail.com>
2024-02-06 18:51:58 +0530

    Login completed

--- app/src/main/java/com/example/fitnessstudio/splash_screen/SplashScreen4.java
+++ app/src/main/java/com/example/fitnessstudio/splash_screen/SplashScreen4.java
@@ -1,8 +1,10 @@
 package com.example.fitnessstudio.splash_screen;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -10,6 +12,8 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.fitnessstudio.R;
+import com.example.fitnessstudio.login_screen.LoginScreen1;
+
 
 @SuppressLint("CustomSplashScreen")
 public class SplashScreen4 extends Fragment {
@@ -17,6 +21,14 @@ public class SplashScreen4 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_splash_screen4, container, false);
+        View view=inflater.inflate(R.layout.fragment_splash_screen4, container, false);
+        AppCompatButton getStartedButton=view.findViewById(R.id.get_started_button_splash_screen4);
+        getStartedButton.setOnClickListener(event->{
+            Intent intent=new Intent(getActivity(), LoginScreen1.class);
+            startActivity(intent);
+            requireActivity().overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
+            requireActivity().finish();
+        });
+        return view;
     }
 }
