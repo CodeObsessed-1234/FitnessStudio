59011ff52a06d9c348e0908bd4a084b86e20fa39
Nitin Dwivedi <nitindwivedi878@gmail.com>
2024-04-02 19:39:00 +0530

    changed and share option

--- app/src/main/java/com/example/fitnessstudio/user_interface_fragments/MainUserInterface.java
+++ app/src/main/java/com/example/fitnessstudio/user_interface_fragments/MainUserInterface.java
@@ -1,6 +1,9 @@
 package com.example.fitnessstudio.user_interface_fragments;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.AlarmClock;
@@ -12,6 +15,10 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.fitnessstudio.R;
@@ -22,6 +29,9 @@ import com.example.fitnessstudio.session.SessionManager;
 import java.util.Base64;
 
 public class MainUserInterface extends Fragment {
+
+	private static final int REQUEST_CODE_PICK_FILE = 1001;
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 													 Bundle savedInstanceState) {
@@ -61,13 +71,19 @@ public class MainUserInterface extends Fragment {
 			if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
 				String updatedUrlUid = Base64.getEncoder()
 				 .encodeToString(SessionManager.getUserId().getBytes());
-
 				Log.d("TAG", "onCreateView: " + updatedUrlUid);
-				Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://fitness-studio-v0.netlify.app/analysis/" + updatedUrlUid));
+				Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://fitness-studio-v01.netlify.app/analysis/" + updatedUrlUid));
 				startActivity(intent);
 			} else {
 				Toast.makeText(getContext(), "Version Error", Toast.LENGTH_SHORT).show();
 			}
+		});
+		LinearLayout share = view.findViewById(R.id.share);
+		share.setOnClickListener(event -> {
+			if (ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+				ActivityCompat.requestPermissions(requireActivity(), new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 100);
+			}
+			startFilePicker();
 		});
 		LinearLayout subscribeButton = view.findViewById(R.id.subscribe);
 		subscribeButton.setOnClickListener(event -> {
@@ -75,5 +91,40 @@ public class MainUserInterface extends Fragment {
 			requireActivity().overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
 		});
 		return view;
+	}
+
+	private void startFilePicker() {
+		Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+		intent.addCategory(Intent.CATEGORY_OPENABLE);
+		intent.setType("application/pdf"); // Specific MIME type for PDF files
+		startActivityForResult(intent, REQUEST_CODE_PICK_FILE);
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		if (requestCode == REQUEST_CODE_PICK_FILE && resultCode == Activity.RESULT_OK && data != null) {
+			Uri selectedFileUri = data.getData();
+			shareFile(selectedFileUri);
+		}
+	}
+
+	private void shareFile(Uri fileUri) {
+		Intent shareIntent = new Intent(Intent.ACTION_SEND);
+		shareIntent.setType("application/pdf"); // Set the MIME type of the file
+		shareIntent.putExtra(Intent.EXTRA_STREAM, fileUri);
+		startActivity(Intent.createChooser(shareIntent, "Share File"));
+	}
+
+	@Override
+	public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+		if (requestCode == 100) {
+			// Check if the permission is granted
+			if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+				startFilePicker();
+			} else {
+				Toast.makeText(getContext(), "pdf will not be able to share", Toast.LENGTH_SHORT).show();
+			}
+		}
 	}
 }
